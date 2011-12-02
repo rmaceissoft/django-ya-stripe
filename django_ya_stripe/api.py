3fe49c9d5996303da41eb4566af65f456e55141c
Reiner Marquez <rmaceissoft@gmail.com>
2011-12-01 21:03:41 -0500

    fixed some bugs when redunf an object

--- django_ya_stripe/api.py
+++ django_ya_stripe/api.py
@@ -12,22 +12,29 @@ def sync_charge_object(obj, stripe_charge):
 
 
 def refund_object(obj, amount_to_refund=None):
+    """
+    refund a given amount of django model instance specified
+    returns:
+        a tuple (SyncChargedObject instance, refund success?)
+    """
     try:
         sync_charged_object = SyncChargedObject.objects.for_model(obj).get()
     except SyncChargedObject.DoesNotExists, ex:
-        pass
+        raise ex
     else:
         try:
             if not amount_to_refund:
-                amount_to_refund = sync_charged_object.amount_charge-sync_charged_object.amount_refunded
-            stripe_charge = stripe.Charge.retrieve(settings.STRIPE_SECRET)
-            stripe_charge.refund(dict(id=sync_charged_object.stripe_charge_id, amount=int(amount_to_refund*100)))
+                amount_to_refund = sync_charged_object.amount_by_refund
+            stripe_charge = stripe.Charge.retrieve(sync_charged_object.stripe_charge_id, settings.STRIPE_SECRET)
+            stripe_charge.refund(amount=int(amount_to_refund*100))
         except Exception, ex:
             logging.error(ex)
+            return sync_charged_object, False
         else:
             sync_charged_object.amount_refunded += amount_to_refund
             sync_charged_object.last_updated = datetime.datetime.now()
             sync_charged_object.save()
+            return sync_charged_object, True
 
         
 
